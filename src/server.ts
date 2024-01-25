4a1b010dff6b8688258c9aa41b9be8da6ba4c5b6
Nikolai Kushner <nickolaikushner@gmail.com>
2024-01-25 13:31:29 +0100

    update-mocks-server (#26)
    * update mocks server
    * fit types error

--- src/server.ts
+++ src/server.ts
@@ -118,64 +118,68 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
     return createServer({
         environment,
         namespace: 'api',
+
         seeds(server) {
             server.db.loadData(isSeed ? dbTemplate : emptyDbTemplate);
         },
+
         routes() {
             this.get(`${BASE_PATH}/shutdown`, () =>  {
                 this.shutdown();
                 return 'shutdown';
             });
 
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/events`, (schema) => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/schema/events`, (schema) => {
                 return { data: schema.db.events }
             }, { timing: getRandomTiming() })
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/events/:event_id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/schema/events/:event_id`, (schema, request) => {
                 const customEvent = JSON.parse(request.requestBody)
 
                 return schema.db.events.update(request.params.event_id, customEvent)
             }, { timing: getRandomTiming() })
 
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/custom-events`, (schema) => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/schema/custom-events`, (schema) => {
                 return {
                     data: schema.db.customEvents.map(item => ({...item, id: Number(item.id)})),
                 };
             })
 
-            this.delete(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/custom-events/:event_id`, (schema, request) => {
+            this.delete(`${BASE_PATH}/v1/projects/:project_id/schema/custom-events/:event_id`, (schema, request) => {
                 return EMPTY_SUCCESS_RES;
             })
 
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/custom-events`, (schema, request) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/schema/custom-events`, (schema, request) => {
                 const customEvents = JSON.parse(request.requestBody)
 
                 return schema.db.customEvents.insert(customEvents)
             }, { timing: getRandomTiming() })
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/custom-events/:event_id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/schema/custom-events/:event_id`, (schema, request) => {
                 const customEvent = JSON.parse(request.requestBody)
                 schema.db.customEvents.update(request.params.event_id, customEvent)
 
                 return schema.db.customEvents
             })
 
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/event-records/search`, (schema, request) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/event-records/search`, (schema, request) => {
                 return {
-                    events: schema.db.liveStreamMocks,
-                };
+                    data: schema.db.liveStreamMocks
+                }
             })
 
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/event-properties`, (schema) => {
-                return { data: schema.db.eventProperties }
+            this.get(`${BASE_PATH}/v1/projects/:project_id/schema/event-properties`, (schema) => {
+                return {
+                    data: schema.db.eventProperties
+                }
             })
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/event-properties/:property_id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/schema/event-properties/:property_id`, (schema, request) => {
                 const property = JSON.parse(request.requestBody)
                 return schema.db.eventProperties.update(request.params.property_id, property)
             })
 
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/user-properties`, (schema) => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/schema/user-properties`, (schema) => {
                 return {
                     data: schema.db.userProperties.map(item => ({
                         ...item,
@@ -184,12 +188,12 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 };
             }, { timing: getRandomTiming() })
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/user-properties/:property_id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/schema/user-properties/:property_id`, (schema, request) => {
                 const property = JSON.parse(request.requestBody)
                 return schema.db.userProperties.update(request.params.property_id, property)
             }, { timing: getRandomTiming() })
 
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/schema/custom-properties`, () => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/schema/custom-properties`, () => {
                 return {
                     events: this.schema.db.customProperties,
                 };
@@ -223,7 +227,7 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 ];
             });
 
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/property-values`, (_, request) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/property-values`, (_, request) => {
                 const propertyName = request.queryParams?.property_name
                 let values = []
 
@@ -236,11 +240,7 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 return { data: values }
             });
 
-            this.get('/chart', (): any[] => {
-                return splineChartMocks;
-            });
-
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/reports`, (schema) => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/reports`, (schema) => {
                 return {
                     data: schema.db.reports.map(item => ({
                         ...item,
@@ -250,7 +250,7 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 }
             })
 
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/reports`, (schema, request) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/reports`, (schema, request) => {
                 const body = JSON.parse(request.requestBody);
 
                 return schema.db.reports.insert({
@@ -259,21 +259,21 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 })
             }, { timing: getRandomTiming() })
 
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/reports/:report_id`, (schema, request) => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/reports/:report_id`, (schema, request) => {
                 return schema.db.reports.find(request.params.report_id);
             })
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/reports/:report_id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/reports/:report_id`, (schema, request) => {
                 const body = JSON.parse(request.requestBody);
                 return schema.db.reports.update(request.params.report_id, body)
             }, { timing: getRandomTiming() })
 
-            this.delete(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/reports/:report_id`, (schema, request) => {
+            this.delete(`${BASE_PATH}/v1/projects/:project_id/reports/:report_id`, (schema, request) => {
                 schema.db.reports.remove(request.params.report_id)
                 return request.params.report_id;
             })
 
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/queries/event-segmentation`, (_, request) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/queries/event-segmentation`, (_, request) => {
                 const body = JSON.parse(request.requestBody);
 
                 if (body.events?.length || body?.segments?.length) {
@@ -285,7 +285,7 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 }
             })
 
-            this.post(`${BASE_PATH}/organizations/:organization_id/projects/:project_id/queries/funnel`, (schema, request) => {
+            this.post(`${BASE_PATH}/projects/:project_id/queries/funnel`, (schema, request) => {
                 return funnelsMocks
             })
 
@@ -309,17 +309,14 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 return TokensResponse
             }, { timing: getRandomTiming() })
 
-            /**
-             * dashboards
-             */
-            this.get(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/dashboards`, (schema) => {
+            this.get(`${BASE_PATH}/v1/projects/:project_id/dashboards`, (schema) => {
                 return {
                     data: schema.db.dashboards,
                     meta: {}
                 }
             })
 
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/dashboards`, (schema, request) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/dashboards`, (schema, request) => {
                 const body = JSON.parse(request.requestBody)
                 return schema.db.dashboards.insert({
                     id: nanoid(),
@@ -327,12 +324,12 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                 })
             }, { timing: getRandomTiming() })
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/dashboards/:dashboard_id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/dashboards/:dashboard_id`, (schema, request) => {
                 const requestBody = JSON.parse(request.requestBody)
                 return schema.db.dashboards.update(request.params.dashboard_id, requestBody)
             }, { timing: getRandomTiming() })
 
-            this.delete(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/dashboards/:dashboard_id`, (schema, request) => {
+            this.delete(`${BASE_PATH}/v1/projects/:project_id/dashboards/:dashboard_id`, (schema, request) => {
                 schema.db.dashboards.remove(request.params.dashboard_id)
                 return request.params.dashboard_id;
             }, { timing: getRandomTiming() })
@@ -343,13 +340,13 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
              * post
              * put
              */
-            this.post(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/group-records/search`, (schema) => {
+            this.post(`${BASE_PATH}/v1/projects/:project_id/group-records/search`, (schema) => {
                 return {
                     data: schema.db.groupRecords,
                 };
             }, { timing: getRandomTiming() });
 
-            this.put(`${BASE_PATH}/v1/organizations/:organization_id/projects/:project_id/group-records/:id`, (schema, request) => {
+            this.put(`${BASE_PATH}/v1/projects/:project_id/group-records/:id`, (schema, request) => {
                 return schema.db.groupRecords.update(request.params.id, JSON.parse(request.requestBody));
             }, { timing: getRandomTiming() });
             /**
@@ -382,7 +379,7 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                         HttpStatusCode.BAD_REQUEST,
                         EMPTY_HEADER_RESPONSE,
                         getErrorResponse([['name', 'Name is incorrect']]))
-                
+
                 schema.db.profile.update(userId, { name })
                 return EMPTY_SUCCESS_RES
             }, { timing: 1000 })
@@ -428,7 +425,7 @@ export default function ({ environment = 'development', isSeed = true } = {}) {
                                 'message': Stub.ERROR
                             }
                         })
-                
+
                 if (password.toLowerCase() === Stub.ERROR && newPassword.toLowerCase() === Stub.ERROR)
                     return new Response(
                         HttpStatusCode.BAD_REQUEST,
