5ec820e29c8ecf01e0414c2ff24a9aa27e50ff72
Nikolay Kushner <nickolaikushner@gmail.com>
2024-01-27 22:13:12 +0100

    agregare => aggregate

--- src/components/events/Events/Query.vue
+++ src/components/events/Events/Query.vue
@@ -1,295 +1,286 @@
 <template>
-    <div class="queries pf-l-flex">
-        <div class="pf-c-action-list">
-            <div class="pf-c-action-list__item min-w-50 pf-u-text-align-right">
-                query
-            </div>
-            <div class="pf-c-action-list__item">
-                <Select
-                    v-if="item.queryRef"
-                    :items="lexiconStore.eventsQueries"
-                    :selected="item.queryRef"
-                    :width-auto="true"
-                    @select="changeQuery"
-                >
-                    <UiButton class="pf-m-secondary">
-                        {{ querySelectorName }}
-                    </UiButton>
-                </Select>
-                <Select
-                    v-else
-                    :items="lexiconStore.eventsQueries"
-                    :update-open="updateOpen"
-                    @select="changeQuery"
-                >
-                    <UiButton
-                        :before-icon="'fas fa-plus-circle'"
-                        class="pf-m-primary"
-                        type="button"
-                    >
-                        Select Query
-                    </UiButton>
-                </Select>
-            </div>
+  <div class="queries pf-l-flex">
+    <div class="pf-c-action-list">
+      <div class="pf-c-action-list__item min-w-50 pf-u-text-align-right">query</div>
+      <div class="pf-c-action-list__item">
+        <Select
+          v-if="item.queryRef"
+          :items="lexiconStore.eventsQueries"
+          :selected="item.queryRef"
+          :width-auto="true"
+          @select="changeQuery"
+        >
+          <UiButton class="pf-m-secondary">
+            {{ querySelectorName }}
+          </UiButton>
+        </Select>
+        <Select
+          v-else
+          :items="lexiconStore.eventsQueries"
+          :update-open="updateOpen"
+          @select="changeQuery"
+        >
+          <UiButton :before-icon="'fas fa-plus-circle'" class="pf-m-primary" type="button">
+            {{ $t('events.selectQuery') }}
+          </UiButton>
+        </Select>
+      </div>
 
-            <PropertySelect
-                v-if="showProperty"
-                :event-ref="eventRef"
-                :selected="propRef"
-                :width-auto="true"
-                @select="changeProperty"
-            >
-                <UiButton
-                    :before-icon="!propRef ? 'fas fa-plus-circle' : ''"
-                    :class="{
-                        'pf-m-secondary': propRef,
-                        'pf-m-primary': !propRef,
-                    }"
-                >
-                    {{ propertySelectButtonText }}
-                </UiButton>
-            </PropertySelect>
-            <div
-                v-if="showAggregateText"
-                class="pf-c-action-list__item pf-u-text-align-right"
-            >
-                {{ aggregateString }}
-            </div>
-            <div
-                v-if="showOnlyAggregate"
-                class="pf-c-action-list__item"
-            >
-                <Select
-                    :items="lexiconStore.eventsQueryAggregates"
-                    :selected="selectedAggregateRef"
-                    :show-search="false"
-                    :width-auto="true"
-                    @select="changeQueryAggregate"
-                >
-                    <UiButton
-                        :before-icon="!selectedAggregateRef ? 'fas fa-plus-circle' : ''"
-                        :class="{
-                            'pf-m-secondary': selectedAggregateRef,
-                            'pf-m-primary': !selectedAggregateRef,
-                        }"
-                    >
-                        {{ selectAggregateName }}
-                    </UiButton>
-                </Select>
-            </div>
-            <div
-                v-if="showGroupAggregate"
-                class="pf-c-action-list__item"
-            >
-                <Select
-                    :items="lexiconStore.eventsQueryAggregates"
-                    :selected="selectedGroupAggregateRef"
-                    :show-search="false"
-                    :width-auto="true"
-                    @select="changeQueryGroupAggregate"
-                >
-                    <UiButton
-                        :before-icon="!selectedGroupAggregateRef ? 'fas fa-plus-circle' : ''"
-                        :class="{
-                            'pf-m-secondary': selectedGroupAggregateRef,
-                            'pf-m-primary': !selectedGroupAggregateRef,
-                        }"
-                    >
-                        {{ selectedGroupAggregateName }}
-                    </UiButton>
-                </Select>
-            </div>
-            <div
-                v-if="queryInfo?.hasValue"
-                class="pf-c-action-list__item"
-            >
-                <UiInput @input="changeFormula" />
-            </div>
-            <div
-                v-if="propertySelectError"
-                class="pf-c-action-list__item pf-u-mt-xs"
-            >
-                <VTooltip popper-class="ui-hint">
-                    <UiIcon
-                        class="pf-u-warning-color-100"
-                        icon="fas fa-exclamation-triangle"
-                    />
-                    <template #popper>
-                        {{ $t('events.query.error') }}
-                    </template>
-                </VTooltip>
-            </div>
-            <div
-                v-if="!noDelete"
-                class="pf-c-action-list__item queries__control-item"
-                @click="removeQuery"
-            >
-                <VTooltip popper-class="ui-hint">
-                    <UiIcon icon="fas fa-times" />
-                    <template #popper>
-                        {{ $t('events.query.removeQuery') }}
-                    </template>
-                </VTooltip>
-            </div>
-        </div>
+      <PropertySelect
+        v-if="showProperty"
+        :event-ref="eventRef"
+        :selected="propRef"
+        :width-auto="true"
+        @select="changeProperty"
+      >
+        <UiButton
+          :before-icon="!propRef ? 'fas fa-plus-circle' : ''"
+          :class="{
+            'pf-m-secondary': propRef,
+            'pf-m-primary': !propRef,
+          }"
+        >
+          {{ propertySelectButtonText }}
+        </UiButton>
+      </PropertySelect>
+      <div v-if="showAggregateText" class="pf-c-action-list__item pf-u-text-align-right">
+        {{ aggregateString }}
+      </div>
+      <div v-if="showOnlyAggregate" class="pf-c-action-list__item">
+        <Select
+          :items="lexiconStore.eventsQueryAggregates"
+          :selected="selectedAggregateRef"
+          :show-search="false"
+          :width-auto="true"
+          @select="changeQueryAggregate"
+        >
+          <UiButton
+            :before-icon="!selectedAggregateRef ? 'fas fa-plus-circle' : ''"
+            :class="{
+              'pf-m-secondary': selectedAggregateRef,
+              'pf-m-primary': !selectedAggregateRef,
+            }"
+          >
+            {{ selectAggregateName }}
+          </UiButton>
+        </Select>
+      </div>
+      <div v-if="showGroupAggregate" class="pf-c-action-list__item">
+        <Select
+          :items="lexiconStore.eventsQueryAggregates"
+          :selected="selectedGroupAggregateRef"
+          :show-search="false"
+          :width-auto="true"
+          @select="changeQueryGroupAggregate"
+        >
+          <UiButton
+            :before-icon="!selectedGroupAggregateRef ? 'fas fa-plus-circle' : ''"
+            :class="{
+              'pf-m-secondary': selectedGroupAggregateRef,
+              'pf-m-primary': !selectedGroupAggregateRef,
+            }"
+          >
+            {{ selectedGroupAggregateName }}
+          </UiButton>
+        </Select>
+      </div>
+      <div v-if="queryInfo?.hasValue" class="pf-c-action-list__item">
+        <UiInput @input="changeFormula" />
+      </div>
+      <div v-if="propertySelectError" class="pf-c-action-list__item pf-u-mt-xs">
+        <VTooltip popper-class="ui-hint">
+          <UiIcon class="pf-u-warning-color-100" icon="fas fa-exclamation-triangle" />
+          <template #popper>
+            {{ $t('events.query.error') }}
+          </template>
+        </VTooltip>
+      </div>
+      <div
+        v-if="!noDelete"
+        class="pf-c-action-list__item queries__control-item"
+        @click="removeQuery"
+      >
+        <VTooltip popper-class="ui-hint">
+          <UiIcon icon="fas fa-times" />
+          <template #popper>
+            {{ $t('events.query.removeQuery') }}
+          </template>
+        </VTooltip>
+      </div>
     </div>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { computed } from 'vue';
-import { EventQuery } from '@/stores/eventSegmentation/events';
-import { useLexiconStore } from '@/stores/lexicon';
-import { useEventsStore, Events } from '@/stores/eventSegmentation/events';
-import { EventRef, EventQueryRef, EventsQuery, PropertyRef } from '@/types/events';
-import { AggregateRef } from '@/types/aggregate';
-import { Item } from '@/components/Select/SelectTypes';
-import UiInput from '@/components/uikit/UiInput.vue';
-import Select from '@/components/Select/Select.vue';
-import PropertySelect from '@/components/events/PropertySelect.vue';
-import UiButton from '@/components/uikit/UiButton.vue';
-import usei18n from '@/hooks/useI18n';
+import { computed } from 'vue'
+import { EventQuery } from '@/stores/eventSegmentation/events'
+import { useLexiconStore } from '@/stores/lexicon'
+import { useEventsStore, Events } from '@/stores/eventSegmentation/events'
+import { EventRef, EventQueryRef, EventsQuery, PropertyRef } from '@/types/events'
+import { AggregateRef } from '@/types/aggregate'
+import { Item } from '@/components/Select/SelectTypes'
+import UiInput from '@/components/uikit/UiInput.vue'
+import Select from '@/components/Select/Select.vue'
+import PropertySelect from '@/components/events/PropertySelect.vue'
+import UiButton from '@/components/uikit/UiButton.vue'
+import usei18n from '@/hooks/useI18n'
 
-const eventsStore: Events = useEventsStore();
-const lexiconStore = useLexiconStore();
-const { t } = usei18n();
+const eventsStore: Events = useEventsStore()
+const lexiconStore = useLexiconStore()
+const { t } = usei18n()
 
 const props = defineProps<{
-    eventRef: EventRef;
-    item: EventQuery;
-    index: number;
-    updateOpen?: boolean;
-    noDelete?: boolean;
-}>();
+  eventRef: EventRef
+  item: EventQuery
+  index: number
+  updateOpen?: boolean
+  noDelete?: boolean
+}>()
 
 const emit = defineEmits<{
-    (e: 'removeQuery', index: number): void;
-    (e: 'changeQuery', queryIdx: number, queryRef: EventQueryRef): void;
-}>();
+  (e: 'removeQuery', index: number): void
+  (e: 'changeQuery', queryIdx: number, queryRef: EventQueryRef): void
+}>()
 
 const queryInfo = computed((): EventsQuery | undefined => {
-    return lexiconStore.findQuery(props.item.queryRef);
-});
+  return lexiconStore.findQuery(props.item.queryRef)
+})
 
 const propRef = computed((): PropertyRef | undefined => {
-    return props?.item?.queryRef?.propRef;
-});
+  return props?.item?.queryRef?.propRef
+})
 
-const showProperty = computed(() => queryInfo.value?.hasProperty);
+const showProperty = computed(() => queryInfo.value?.hasProperty)
 
 const property = computed(() => {
-    return props.item?.queryRef?.propRef ? lexiconStore.property(props.item?.queryRef?.propRef) : null;
-});
+  return props.item?.queryRef?.propRef ? lexiconStore.property(props.item?.queryRef?.propRef) : null
+})
 
-const propertySelectError = computed(() => props.item?.queryRef?.propRef && !property.value);
+const propertySelectError = computed(() => props.item?.queryRef?.propRef && !property.value)
 
-const propertyName = computed((): string => (property.value?.name || ''));
+const propertyName = computed((): string => property.value?.name || '')
 
 const propertySelectButtonText = computed(() => {
-    return propertyName.value || props.item?.queryRef?.propRef?.name || props.item?.queryRef?.propRef?.id || t('events.selectProperty');
-});
+  return (
+    propertyName.value ||
+    props.item?.queryRef?.propRef?.name ||
+    props.item?.queryRef?.propRef?.id ||
+    t('events.selectProperty')
+  )
+})
 
 const showOnlyAggregate = computed(() => {
-    return queryInfo.value?.hasAggregate && !queryInfo.value?.hasProperty;
-});
+  return queryInfo.value?.hasAggregate && !queryInfo.value?.hasProperty
+})
 
 const showGroupAggregate = computed(() => {
-    return queryInfo.value?.hasGroupAggregate;
-});
+  return queryInfo.value?.hasGroupAggregate
+})
 
 const showAggregateText = computed(() => {
-    return showGroupAggregate.value || showOnlyAggregate.value;
-});
+  return showGroupAggregate.value || showOnlyAggregate.value
+})
 
 const selectedAggregate = computed((): Item<EventQueryRef, null> | undefined => {
-    return props.item?.queryRef?.typeAggregate ? lexiconStore.eventsQueryAggregates.find(item => props.item?.queryRef?.typeAggregate === item.item.typeAggregate) : undefined;
-});
+  return props.item?.queryRef?.typeAggregate
+    ? lexiconStore.eventsQueryAggregates.find(
+        item => props.item?.queryRef?.typeAggregate === item.item.typeAggregate
+      )
+    : undefined
+})
 
 const selectAggregateName = computed((): string => {
-    return selectedAggregate.value?.name || 'Select agregate';
-});
+  return selectedAggregate.value?.name || t('events.selectAggregate')
+})
 
 const selectedAggregateRef = computed((): AggregateRef | undefined => {
-    return selectedAggregate.value?.item as AggregateRef || undefined;
-});
+  return (selectedAggregate.value?.item as AggregateRef) || undefined
+})
 
 const selectedGroupAggregate = computed((): Item<EventQueryRef, null> | undefined => {
-    return props.item?.queryRef?.typeGroupAggregate ?
-        lexiconStore.eventsQueryAggregates.find(item => props.item?.queryRef?.typeGroupAggregate === item.item.typeAggregate) :
-        undefined;
-});
+  return props.item?.queryRef?.typeGroupAggregate
+    ? lexiconStore.eventsQueryAggregates.find(
+        item => props.item?.queryRef?.typeGroupAggregate === item.item.typeAggregate
+      )
+    : undefined
+})
 
 const selectedGroupAggregateRef = computed((): AggregateRef | undefined => {
-    return selectedGroupAggregate.value?.item as AggregateRef || undefined;
-});
+  return (selectedGroupAggregate.value?.item as AggregateRef) || undefined
+})
 
 const selectedGroupAggregateName = computed((): string => {
-    return selectedGroupAggregate.value?.name || 'Select agregate';
-});
+  return selectedGroupAggregate.value?.name || t('events.selectAggregate')
+})
 
 const querySelectorName = computed((): string => {
-    return showProperty.value && props.item.queryRef ? `${selectAggregateName.value} of property` : queryInfo.value?.displayName || '';
-});
+  return showProperty.value && props.item.queryRef
+    ? `${selectAggregateName.value} of property`
+    : queryInfo.value?.displayName || ''
+})
 
 const aggregateString = computed((): string => {
-    return `per ${eventsStore.group}, agregate by`;
-});
+  return t('events.perGroupAggregateBy', {
+    group: eventsStore.group,
+  })
+})
 
 const changeQueryAggregate = (payload: AggregateRef) => {
-    if (props.item.queryRef) {
-        emit('changeQuery', props.index, {
-            ...props.item.queryRef,
-            ...payload
-        });
-    }
-};
+  if (props.item.queryRef) {
+    emit('changeQuery', props.index, {
+      ...props.item.queryRef,
+      ...payload,
+    })
+  }
+}
 
 const changeQueryGroupAggregate = (payload: AggregateRef) => {
-    if (props.item.queryRef) {
-        emit('changeQuery', props.index, {
-            ...props.item.queryRef,
-            typeGroupAggregate: payload.typeAggregate,
-        });
-    }
-};
+  if (props.item.queryRef) {
+    emit('changeQuery', props.index, {
+      ...props.item.queryRef,
+      typeGroupAggregate: payload.typeAggregate,
+    })
+  }
+}
 
 const changeQuery = (payload: EventQueryRef) => {
-    emit('changeQuery', props.index, payload);
+  emit('changeQuery', props.index, payload)
 }
 
 const removeQuery = () => {
-    emit('removeQuery', props.index);
+  emit('removeQuery', props.index)
 }
 
 const changeProperty = (payload: PropertyRef) => {
-    if (props.item.queryRef) {
-        emit('changeQuery', props.index, {
-            ...props.item.queryRef,
-            propRef: payload,
-        });
-    }
-};
+  if (props.item.queryRef) {
+    emit('changeQuery', props.index, {
+      ...props.item.queryRef,
+      propRef: payload,
+    })
+  }
+}
 
 const changeFormula = (value: string) => {
-    if (props.item.queryRef) {
-        emit('changeQuery', props.index, {
-            ...props.item.queryRef,
-            value,
-        })
-    }
+  if (props.item.queryRef) {
+    emit('changeQuery', props.index, {
+      ...props.item.queryRef,
+      value,
+    })
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .queries {
-    &:hover {
-        .queries__control-item {
-            opacity: 1;
-        }
+  &:hover {
+    .queries__control-item {
+      opacity: 1;
     }
+  }
 
-    &__control-item {
-        opacity: 0;
-        cursor: pointer;
-    }
+  &__control-item {
+    opacity: 0;
+    cursor: pointer;
+  }
 }
 </style>
