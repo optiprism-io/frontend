66d16bd3c1a8a629869731ca32587470af16a66b
Nikolay Kushner <nickolaikushner@gmail.com>
2024-01-28 17:22:23 +0100

    add  QueryCountPerGroup and QueryFormula formatted value string

--- src/helpers/reportTableHelper.ts
+++ src/helpers/reportTableHelper.ts
@@ -3,16 +3,44 @@ import {
   ReportType,
   EventSegmentation,
   EventSegmentationEvent,
-  EventSegmentationEventAllOfQueries,
   QuerySimpleTypeEnum,
   QueryAggregatePropertyTypeEnum,
+  QueryAggregatePropertyPerGroupTypeEnum,
+  PropertyType,
+  QueryCountPerGroupTypeEnum,
+  QueryFormulaTypeEnum,
 } from '@/api'
 import useI18n from '@/hooks/useI18n'
 import { useLexiconStore } from '@/stores/lexicon'
+import { useEventsStore } from '@/stores/eventSegmentation/events'
+
+const getProperyNameString = (propertyId: number, propertyType: PropertyType) => {
+  const lexiconStore = useLexiconStore()
+
+  const property = lexiconStore.property({
+    id: propertyId,
+    type: propertyType,
+  })
+
+  return property?.name || propertyId
+}
+
+const getAggregateString = (key: string) => {
+  const { t } = useI18n()
+
+  return t(`events.aggregates.${key}`)
+}
+
+const getAggregatePropertyString = (key: string) => {
+  const { t } = useI18n()
+
+  return t(`events.aggregateProperty.${key}`)
+}
 
 export const getQueryFormattedValue = (value: string, report: Report) => {
   const { t } = useI18n()
   const lexiconStore = useLexiconStore()
+  const eventsStore = useEventsStore()
 
   if (report.type === ReportType.EventSegmentation) {
     const indexes = value.split('_')
@@ -23,7 +51,7 @@ export const getQueryFormattedValue = (value: string, report: Report) => {
     const events = queryReport.events || []
 
     const reportEvent = events[eventId] as EventSegmentationEvent
-    const indexQuery = (reportEvent?.queries || [])[queryId] as EventSegmentationEventAllOfQueries
+    const indexQuery = (reportEvent?.queries || [])[queryId]
 
     if (reportEvent && indexQuery) {
       const activeEvent = reportEvent.eventId
@@ -37,22 +65,39 @@ export const getQueryFormattedValue = (value: string, report: Report) => {
         case QuerySimpleTypeEnum.DailyActiveGroups:
         case QuerySimpleTypeEnum.MonthlyActiveGroups:
         case QuerySimpleTypeEnum.WeeklyActiveGroups:
-          resultString += `, ${t(`events.query.${indexQuery.type}`)}`
+          resultString += ` (${t(`events.query.${indexQuery.type}`)})`
           break
         case QueryAggregatePropertyTypeEnum.AggregateProperty:
-          if (indexQuery.aggregate) {
-            resultString += `, ${t(`events.aggregateProperty.${indexQuery.aggregate}`)}`
+          if (indexQuery.aggregate && indexQuery.propertyId && indexQuery.propertyType) {
+            resultString += ` (${getAggregatePropertyString(indexQuery.aggregate)}
+            ${getProperyNameString(indexQuery.propertyId, indexQuery.propertyType)})`
+          }
+          break
+        case QueryAggregatePropertyPerGroupTypeEnum.AggregatePropertyPerGroup:
+          if (indexQuery.aggregatePerGroup) {
+            resultString += `, ${getAggregateString(indexQuery.aggregatePerGroup)}`
 
-            if (indexQuery.propertyId && indexQuery.propertyType) {
-              const property = lexiconStore.property({
-                id: indexQuery.propertyId,
-                type: indexQuery.propertyType,
-              })
-              resultString += `(${property?.name || indexQuery.propertyId})`
+            if (indexQuery.aggregate && indexQuery.propertyId && indexQuery.propertyType) {
+              resultString += ` (${getAggregatePropertyString(indexQuery.aggregate)}
+                 ${getProperyNameString(indexQuery.propertyId, indexQuery.propertyType)}
+                 ${t('events.per')} ${eventsStore.group})`
             }
           }
           break
-        // TODO
+        case QueryCountPerGroupTypeEnum.CountPerGroup:
+          if (indexQuery.aggregate) {
+            resultString += `,
+                ${getAggregateString(indexQuery.aggregate)} (
+                ${getAggregateString(indexQuery.type)} ${t('events.per')} ${eventsStore.group}
+              )`
+          }
+          break
+        case QueryFormulaTypeEnum.Formula:
+          if (indexQuery.formula) {
+            resultString += `, ${getAggregateString(indexQuery.type)}
+            ( ${indexQuery.formula} )`
+          }
+          break
         default:
           resultString += `(${value})`
       }
