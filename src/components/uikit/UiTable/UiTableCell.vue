b7d9cfa156fbebf71b77b5dfa4f6cbe80be80356
kkulebaev <konstantinkulebaev@gmail.com>
2024-02-05 13:05:41 +0400

    feat(table): add human readable format for all cells with type number

--- src/components/uikit/UiTable/UiTableCell.vue
+++ src/components/uikit/UiTable/UiTableCell.vue
@@ -5,11 +5,14 @@
             'pf-u-text-nowrap': props.nowrap,
         }"
     >
-        {{ props.title }}
+        {{ tableCellVal }}
     </div>
 </template>
 
 <script lang="ts" setup>
+import { computed } from 'vue'
+import { humanReadable } from '@/utils/humanReadable'
+import { isNumber } from 'lodash'
 
 type Props = {
     title: string | number
@@ -18,4 +21,6 @@ type Props = {
 }
 
 const props = defineProps<Props>()
+
+const tableCellVal = computed(() => isNumber(props.title) ? humanReadable(props.title) : props.title)
 </script>
