fe45316f1df078eb083ca907dbdc282fea038cea
kkulebaev <konstantinkulebaev@gmail.com>
2024-02-05 13:06:35 +0400

    style: prettier fmt UiTableCell

--- src/components/uikit/UiTable/UiTableCell.vue
+++ src/components/uikit/UiTable/UiTableCell.vue
@@ -1,12 +1,12 @@
 <template>
-    <div
-        class="ui-table-cell"
-        :class="{
-            'pf-u-text-nowrap': props.nowrap,
-        }"
-    >
-        {{ tableCellVal }}
-    </div>
+  <div
+    class="ui-table-cell"
+    :class="{
+      'pf-u-text-nowrap': props.nowrap,
+    }"
+  >
+    {{ tableCellVal }}
+  </div>
 </template>
 
 <script lang="ts" setup>
@@ -15,12 +15,14 @@ import { humanReadable } from '@/utils/humanReadable'
 import { isNumber } from 'lodash'
 
 type Props = {
-    title: string | number
-    value?: string | number
-    nowrap?: boolean
+  title: string | number
+  value?: string | number
+  nowrap?: boolean
 }
 
 const props = defineProps<Props>()
 
-const tableCellVal = computed(() => isNumber(props.title) ? humanReadable(props.title) : props.title)
+const tableCellVal = computed(() =>
+  isNumber(props.title) ? humanReadable(props.title) : props.title
+)
 </script>
