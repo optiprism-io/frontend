1618d07c5717e0dc4012e15b4e620b043de7252c
kkulebaev <konstantinkulebaev@gmail.com>
2024-02-28 13:12:25 +0400

    feat(uikit): created UiPageSidebar

---
+++ src/components/uikit/UiPageSidebar.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="pf-c-page__sidebar">
+    <div class="pf-c-page__sidebar-body">
+      <nav class="pf-c-nav">
+        <ul class="pf-c-nav__list">
+          <li v-for="(item, idx) in items" :key="idx" class="pf-c-nav__item">
+            <RouterLink
+              :to="item.to"
+              class="pf-c-nav__link"
+              active-class="pf-m-current"
+              :class="{ 'pf-m-current': item.isActive }"
+            >
+              {{ item.title }}
+            </RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import { RouteLocationMatched, RouteLocationRaw } from 'vue-router'
+
+interface NavItem {
+  title: string
+  to: RouteLocationMatched | RouteLocationRaw
+  isActive?: boolean
+}
+
+interface IProps {
+  items: NavItem[]
+}
+
+defineProps<IProps>()
+</script>
