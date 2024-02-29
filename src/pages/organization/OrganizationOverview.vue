5b38149e0ff336c57ced2c8394fdef951c3ae88a
kkulebaev <konstantinkulebaev@gmail.com>
2024-03-01 00:37:12 +0400

    feat(organization): add overview page content

--- src/pages/organization/OrganizationOverview.vue
+++ src/pages/organization/OrganizationOverview.vue
@@ -1,11 +1,16 @@
 <template>
   <div>
     <h2>{{ organization.name }}</h2>
+    <UiFormLabel text="Name" for="name">
+      <UiInput :model-value="organization.name" name="name" :disabled="true" />
+    </UiFormLabel>
   </div>
 </template>
 
 <script setup lang="ts">
 import { Organization } from '@/api'
+import UiInput from '@/components/uikit/UiInput.vue'
+import UiFormLabel from '@/components/uikit/UiFormLabel.vue'
 
 interface IProps {
   organization: Organization
