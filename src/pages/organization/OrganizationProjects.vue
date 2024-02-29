1a965d40c76ed817f122ae9f22302b524d499e91
kkulebaev <konstantinkulebaev@gmail.com>
2024-02-29 23:45:07 +0400

    feat(organization): add table for organization projects

--- src/pages/organization/OrganizationProjects.vue
+++ src/pages/organization/OrganizationProjects.vue
@@ -1,9 +1,33 @@
 <template>
   <div>
-    <h1>Organization Projects</h1>
+    <h2>Projects</h2>
+    <UiTable :items="items" :columns="columns" :show-toolbar="false" />
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useProjectsStore } from '@/stores/projects/projects'
+import UiTable from '@/components/uikit/UiTable/UiTable.vue'
+import { Column, Row } from '@/components/uikit/UiTable/UiTable'
+import { computed } from 'vue'
 
-<style scoped lang="scss"></style>
+const projectsStore = useProjectsStore()
+
+const items = computed<Row[]>(() =>
+  projectsStore.projects.map(project =>
+    columns.map(col => ({
+      key: col.value,
+      title: (project as any)[col.value] || 'N/A',
+    }))
+  )
+)
+
+const columns: Column[] = [
+  { title: 'ID', value: 'id' },
+  { title: 'Name', value: 'name' },
+  { title: 'Creator', value: 'creator' },
+  { title: 'Created At', value: 'createdAt' },
+  { title: 'Updated At', value: 'updatedAt' },
+  { title: 'SDK Token', value: 'sdkToken' },
+]
+</script>
