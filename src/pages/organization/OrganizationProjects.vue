fea573cdbba00c5c602943b48398413c325a82ef
kkulebaev <konstantinkulebaev@gmail.com>
2024-02-29 23:55:20 +0400

    feat(organization): add feature creating project

--- src/pages/organization/OrganizationProjects.vue
+++ src/pages/organization/OrganizationProjects.vue
@@ -1,7 +1,23 @@
 <template>
   <div>
-    <h2>Projects</h2>
-    <UiTable :items="items" :columns="columns" :show-toolbar="false" />
+    <div class="pf-l-flex pf-m-justify-content-space-between">
+      <h2>Projects</h2>
+      <UiButton class="pf-m-primary" :before-icon="'fas fa-plus'" @click="setShowCreatePopup(true)">
+        {{ $t('project.createProject') }}
+      </UiButton>
+    </div>
+    <UiTable
+      :items="items"
+      :columns="columns"
+      :is-loading="projectsStore.isLoading"
+      :show-toolbar="false"
+    />
+
+    <CreateProjectPopup
+      v-if="showCreatePopup"
+      @cancel="setShowCreatePopup(false)"
+      @created-project="onCreatedProject"
+    />
   </div>
 </template>
 
@@ -10,6 +26,9 @@ import { useProjectsStore } from '@/stores/projects/projects'
 import UiTable from '@/components/uikit/UiTable/UiTable.vue'
 import { Column, Row } from '@/components/uikit/UiTable/UiTable'
 import { computed } from 'vue'
+import UiButton from '@/components/uikit/UiButton.vue'
+import CreateProjectPopup from '@/components/projects/CreateProjectPopup.vue'
+import { useToggle } from '@vueuse/core'
 
 const projectsStore = useProjectsStore()
 
@@ -30,4 +49,10 @@ const columns: Column[] = [
   { title: 'Updated At', value: 'updatedAt' },
   { title: 'SDK Token', value: 'sdkToken' },
 ]
+
+const [showCreatePopup, setShowCreatePopup] = useToggle(false)
+function onCreatedProject() {
+  setShowCreatePopup(false)
+  projectsStore.getProjectsList()
+}
 </script>
