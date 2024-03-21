5d2dad5a221b184716986b4d1461381b3adcc741
kkulebaev <konstantinkulebaev@gmail.com>
2024-03-21 15:59:24 +0400

    chore: add funnels mocks

--- src/server/services/query.service.ts
+++ src/server/services/query.service.ts
@@ -1,6 +1,9 @@
 import { Server } from 'miragejs'
 import eventSegmentationsMocks from '@/mocks/eventSegmentations/eventSegmentations.json'
-import funnelsMocks from '@/mocks/reports/funnels.json'
+import conversionStepsMocks from '@/mocks/reports/funnels/conversionSteps'
+import conversionOverTimeMocks from '@/mocks/reports/funnels/conversionOverTime'
+import { FunnelQuery, FunnelQueryChartTypeTypeEnum } from '@/api'
+import { CustomError } from '@/server/models/CustomError'
 
 export function queriesRoutes(server: Server) {
   server.post('/projects/:project_id/queries/event-segmentation', (_, request) => {
@@ -15,7 +18,14 @@ export function queriesRoutes(server: Server) {
     }
   })
 
-  server.post(`/projects/:project_id/queries/funnel`, () => {
-    return funnelsMocks
+  server.post(`/projects/:project_id/queries/funnel`, (_, request) => {
+    const body = JSON.parse(request.requestBody) as FunnelQuery
+    const graphType = body.chartType.type
+
+    if (graphType === FunnelQueryChartTypeTypeEnum.ConversionSteps) return conversionStepsMocks
+    if (graphType === FunnelQueryChartTypeTypeEnum.ConversionOverTime)
+      return conversionOverTimeMocks
+
+    return new CustomError().NotImplemented("Don't have mock for this type of graph yet")
   })
 }
