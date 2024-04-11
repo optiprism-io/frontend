18419cca4421d3629658fce73f27281f763a8e98
Konstantin Kulebaev <konstantinkulebaev@gmail.com>
2024-04-11 13:04:34 +0400

    fix: doesn't retry auth/login (#61)
    * fix: circular dependency
    
    * fix: add exception retry for auth methods
    
    * build: add .editorconfig
    
    * build: remove vite timestamp files
    
    * build: remove yarn-error.log
    
    * build: remove package-lock.json
    
    * build: remove env.production
    
    * fix: correct import defineConfig
    
    * build: add to .gitignore some patterns

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import path from 'path'
