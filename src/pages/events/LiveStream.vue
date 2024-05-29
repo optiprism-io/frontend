833b467dd0988c6750a29149c6d8ecc8f61aa99f
Nikolay Kushner <nickolaikushner@gmail.com>
2024-05-29 14:57:45 +0300

    fixes

--- src/pages/events/LiveStream.vue
+++ src/pages/events/LiveStream.vue
@@ -3,6 +3,24 @@
     <template #title>
       {{ $t('events.live_stream.title') }}
     </template>
+    <UiCard class="filter-event-segmentation__item">
+      <UiSelect
+        v-if="selectedGroup?.name"
+        :items="selectGroups"
+        :width-auto="true"
+        :selections="[selectedGroup.id]"
+        @on-select="onSelectGroup"
+      >
+        <template #action>
+          <UiButton
+            :is-link="true"
+            :after-icon="'fas fa-chevron-down'"
+          >
+            {{ selectedGroupByString }}
+          </UiButton>
+        </template>
+      </UiSelect>
+    </UiCard>
     <UiCard class="pf-c-card pf-m-compact pf-u-h-100" :title="$t('events.events')">
       <InputsEventsLiveStream />
     </UiCard>
@@ -15,11 +33,34 @@
 </template>
 
 <script lang="ts" setup>
+import { computed, onMounted } from 'vue'
+import { useLiveStreamStore } from '@/stores/reports/liveStream'
+import { useLexiconStore } from '@/stores/lexicon'
+import usei18n from '@/hooks/useI18n';
+import { useGroup } from '@/hooks/useGroup'
+
 import InputsEventsLiveStream from '@/components/events/LiveStream/InputsEventsLiveStream.vue'
 import TableLiveStream from '@/components/events/LiveStream/TableLiveStream.vue'
 import ToolsLayout from '@/layout/ToolsLayout.vue'
 import UiCard from '@/components/uikit/UiCard/UiCard.vue'
 import UiCardContainer from '@/components/uikit/UiCard/UiCardContainer.vue'
+import UiSelect from '@/components/uikit/UiSelect.vue'
+
+const { t } = usei18n()
+const liveStreamStore = useLiveStreamStore()
+const lexiconStore = useLexiconStore()
+const { selectGroups } = useGroup()
+
+const selectedGroup = computed(() => lexiconStore.groups.find(item => item.id === liveStreamStore.group))
+const selectedGroupByString = computed(() => `${t('common.group', { name: selectedGroup.value?.name })}`)
+
+const onSelectGroup = (id: number) => {
+  liveStreamStore.group = id
+}
+
+onMounted(() => {
+  liveStreamStore.group = 0
+})
 </script>
 
 <style lang="scss"></style>
