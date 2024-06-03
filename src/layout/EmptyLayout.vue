684dc84b44dc6c628d42bf4d401198d35281f380
kkulebaev <konstantinkulebaev@gmail.com>
2024-06-03 12:04:47 +0400

    feat: create empty layout

---
+++ src/layout/EmptyLayout.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="pf-c-page">
+    <main class="pf-c-page__main">
+      <Suspense>
+        <RouterView class="pf-c-page__main-section" />
+        <template #fallback>
+          <UiSpinner class="pf-u-m-auto" size="xl" />
+        </template>
+      </Suspense>
+    </main>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UiSpinner from '@/components/uikit/UiSpinner.vue'
+</script>
