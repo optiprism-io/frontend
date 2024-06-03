095aba5f98c3fb732cf77ca909a53803162e4b8d
kkulebaev <konstantinkulebaev@gmail.com>
2024-06-03 12:04:27 +0400

    feat: create server mock for force update pass

--- src/server/models/Profile.ts
+++ src/server/models/Profile.ts
@@ -6,11 +6,13 @@ export class Profile implements IProfile {
   name: IProfile['name']
   email: IProfile['email']
   timezone: IProfile['timezone']
+  forceUpdatePassword: IProfile['forceUpdatePassword']
 
   constructor(profile: Partial<IProfile> = {}) {
     this.id = profile.id || faker.number.int({ min: 1, max: 1000 })
     this.name = profile.name || faker.person.fullName()
     this.email = profile.email || faker.internet.email()
     this.timezone = profile.timezone || 'utc'
+    this.forceUpdatePassword = profile.forceUpdatePassword ?? true
   }
 }
