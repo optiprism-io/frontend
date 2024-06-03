f4f0f2e68ca1f946154a4f2380cdf65930f5a4ae
kkulebaev <konstantinkulebaev@gmail.com>
2024-06-03 18:51:49 +0400

    feat: add update tokens after force update password

--- src/pages/ForceUpdatePassword.vue
+++ src/pages/ForceUpdatePassword.vue
@@ -9,13 +9,14 @@ import { useRouter } from 'vue-router'
 import ForceUpdatePasswordPopup from '@/components/profile/ForceUpdatePasswordPopup.vue'
 import { useProfileStore } from '@/stores/profile/profile'
 import { pagesMap } from '@/router'
+import { TokensResponse } from '@/api'
 
 const router = useRouter()
 const profileStore = useProfileStore()
 const { setFirstPassword } = profileStore
 
-async function onChangedPassword() {
-  setFirstPassword()
+async function onChangedPassword(tokens: TokensResponse) {
+  setFirstPassword(tokens)
   await router.push({ name: pagesMap.dashboards.name })
 }
 </script>
