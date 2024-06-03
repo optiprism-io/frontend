34d09431d09d729beeb0f1922f4870f18195cd79
kkulebaev <konstantinkulebaev@gmail.com>
2024-06-03 14:35:28 +0400

    feat: add force update password on first login

---
+++ src/components/profile/ForceUpdatePasswordPopup.vue
@@ -0,0 +1,49 @@
+<template>
+  <UiPopupWindow
+    :title="$t('profile.setPassword')"
+    :apply-button="$t('common.save')"
+    :apply-disabled="password.trim() === ''"
+    :closable="false"
+    :apply-loading="isLoading"
+    @apply="setPassword"
+  >
+    <span class="pf-u-display-block pf-u-mb-md">
+      {{ $t('profile.setPasswordText') }}
+    </span>
+
+    <UiFormLabel
+      :text="$t('profile.password')"
+      :required="true"
+      for="password"
+    >
+      <InputPassword v-model="password" :autofocus="true" />
+    </UiFormLabel>
+  </UiPopupWindow>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import UiPopupWindow from '@/components/uikit/UiPopupWindow.vue'
+import UiFormLabel from '@/components/uikit/UiFormLabel.vue'
+import { apiClient } from '@/api/apiClient'
+import InputPassword from '@/components/login/InputPassword.vue'
+import { useMutation } from '@/hooks/useMutation'
+
+const emit = defineEmits<{
+  (e: 'changed-password'): void
+}>()
+
+const password = ref('')
+
+const { mutate: setPassword, isLoading } = useMutation(changePassword)
+
+async function changePassword() {
+  if (!password.value.trim()) return
+
+  await apiClient.profile.setProfilePassword({
+    password: password.value,
+  })
+
+  emit('changed-password')
+}
+</script>
---
+++ src/pages/ForceUpdatePassword.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="pf-l-bullseye">
+    <ForceUpdatePasswordPopup @changed-password="onChangedPassword" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router'
+import ForceUpdatePasswordPopup from '@/components/profile/ForceUpdatePasswordPopup.vue'
+import { useProfileStore } from '@/stores/profile/profile'
+import { pagesMap } from '@/router'
+
+const router = useRouter()
+const profileStore = useProfileStore()
+const { setFirstPassword } = profileStore
+
+async function onChangedPassword() {
+  setFirstPassword()
+  await router.push({ name: pagesMap.dashboards.name })
+}
+</script>
