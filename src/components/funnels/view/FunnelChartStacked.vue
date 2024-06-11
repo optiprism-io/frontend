4b8bc722125c58024b7d5afbfcb5b935d8f2ec73
kkulebaev <konstantinkulebaev@gmail.com>
2024-06-11 11:13:34 +0400

    fix: add percent digit to ratio value

--- src/components/funnels/view/FunnelChartStacked.vue
+++ src/components/funnels/view/FunnelChartStacked.vue
@@ -114,7 +114,7 @@ const update = () => {
                 {
                   ...item,
                   name: uncamelize(secondaryKey),
-                  value: secondaryValue,
+                  value: secondaryValue + ' %',
                 },
               ]
             : []
