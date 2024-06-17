1ea19b1c9c44922a32a0202a3e9d48a5494cca12
kkulebaev <konstantinkulebaev@gmail.com>
2024-06-17 14:22:03 +0400

    fix: layout of steps

--- src/components/funnels/steps/StepItem.vue
+++ src/components/funnels/steps/StepItem.vue
@@ -1,75 +1,66 @@
 <template>
   <div class="pf-l-flex">
-    <CommonIdentifier
-      class="pf-l-flex__item"
-      type="numeric"
-      :index="index"
-    />
-    <div class="pf-l-flex pf-m-column">
-      <div
-        v-for="(event, i) in step.events"
-        :key="event.event.id"
-        class="pf-l-flex pf-m-column"
-      >
-        <UiActionList>
-          <template #main>
-            <EventSelector @select="(value) => editStepEvent(i, value)">
-              <UiButton class="pf-m-secondary">
-                {{ eventName(event.event) }}
-              </UiButton>
+    <div
+      v-for="(event, i) in step.events"
+      :key="event.event.id"
+      class="pf-l-flex pf-u-align-items-center"
+    >
+      <CommonIdentifier
+        class="pf-l-flex__item"
+        type="numeric"
+        :index="index"
+      />
+      <UiActionList>
+        <template #main>
+          <EventSelector @select="value => editStepEvent(i, value)">
+            <UiButton class="pf-m-secondary">
+              {{ eventName(event.event) }}
+            </UiButton>
+          </EventSelector>
+        </template>
+
+        <UiActionListItem v-if="false">
+          <VTooltip class="ui-hint">
+            <EventSelector @select="value => addStepToEvent(index, value)">
+              <UiIcon icon="fas fa-plus" />
             </EventSelector>
-          </template>
+            <template #popper>
+              {{ $t('funnels.steps.addEvent') }}
+            </template>
+          </VTooltip>
+        </UiActionListItem>
 
-          <UiActionListItem v-if="false">
-            <VTooltip class="ui-hint">
-              <EventSelector @select="(value) => addStepToEvent(index, value)">
-                <UiIcon icon="fas fa-plus" />
-              </EventSelector>
-              <template #popper>
-                {{ $t('funnels.steps.addEvent') }}
-              </template>
-            </VTooltip>
-          </UiActionListItem>
+        <UiActionListItem v-if="isShowAddFilter">
+          <VTooltip class="ui-hint">
+            <UiIcon icon="fas fa-filter" @click="addFilterToStep(i)" />
+            <template #popper>
+              {{ $t('funnels.steps.addFilter') }}
+            </template>
+          </VTooltip>
+        </UiActionListItem>
 
-          <UiActionListItem
-            v-if="isShowAddFilter"
-          >
-            <VTooltip class="ui-hint">
-              <UiIcon
-                icon="fas fa-filter"
-                @click="addFilterToStep(i)"
-              />
-              <template #popper>
-                {{ $t('funnels.steps.addFilter') }}
-              </template>
-            </VTooltip>
-          </UiActionListItem>
-
-          <UiActionListItem>
-            <VTooltip class="ui-hint">
-              <UiIcon
-                icon="fas fa-times"
-                @click="deleteEventFromStep(i)"
-              />
-              <template #popper>
-                {{ $t('funnels.steps.removeEvent') }}
-              </template>
-            </VTooltip>
-          </UiActionListItem>
-        </UiActionList>
-        <Filter
-          v-for="(filter, idx) in event.filters"
-          :key="idx"
-          :filter="filter"
-          :event-ref="event.event"
-          :index="idx"
-          @remove-filter="removeFilterForStepEvent(i, idx)"
-          @change-filter-property="(...args) => changeFilterPropertyForStepEvent(i, ...args)"
-          @change-filter-operation="(...args) => changeFilterOperationForEvent(i, ...args)"
-          @add-filter-value="(...args) => addFilterValueForStepEvent(i, ...args)"
-          @remove-filter-value="(...args) => removeFilterValueForStepEvent(i, ...args)"
-        />
-      </div>
+        <UiActionListItem>
+          <VTooltip class="ui-hint">
+            <UiIcon icon="fas fa-times" @click="deleteEventFromStep(i)" />
+            <template #popper>
+              {{ $t('funnels.steps.removeEvent') }}
+            </template>
+          </VTooltip>
+        </UiActionListItem>
+      </UiActionList>
+      <Filter
+        v-for="(filter, idx) in event.filters"
+        :key="idx"
+        class="pf-u-mt-sm"
+        :filter="filter"
+        :event-ref="event.event"
+        :index="idx"
+        @remove-filter="removeFilterForStepEvent(i, idx)"
+        @change-filter-property="(...args) => changeFilterPropertyForStepEvent(i, ...args)"
+        @change-filter-operation="(...args) => changeFilterOperationForEvent(i, ...args)"
+        @add-filter-value="(...args) => addFilterValueForStepEvent(i, ...args)"
+        @remove-filter-value="(...args) => removeFilterValueForStepEvent(i, ...args)"
+      />
     </div>
   </div>
 </template>
