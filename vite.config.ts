f642079abb124efd432c78ed8b1f3fdb9e05cdb1
Konstantin Kulebaev <konstantinkulebaev@gmail.com>
2024-07-13 15:07:23 +0400

    refactor: axios instance create (#96)

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,28 @@ import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { defineConfig } from 'vite'
 
-export default defineConfig({
-  build: {
-    /* https://rollupjs.org/configuration-options/#output-manualchunks */
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          lodash: ['lodash-es'],
-          '@antv/g2': ['@antv/g2'],
-          '@antv/g2plot': ['@antv/g2plot'],
+export default defineConfig(() => {
+  return {
+    build: {
+      /* https://rollupjs.org/configuration-options/#output-manualchunks */
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            lodash: ['lodash-es'],
+            '@antv/g2': ['@antv/g2'],
+            '@antv/g2plot': ['@antv/g2plot'],
+          },
         },
       },
     },
-  },
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  plugins: [vue({}), vueJsx({})],
-  test: {
-    include: ['test/unit/**/*.{test,spec}.ts', 'test/contract/**/*.{test,spec}.ts'],
-  },
+    plugins: [vue({}), vueJsx({})],
+    test: {
+      include: ['test/unit/**/*.{test,spec}.ts', 'test/contract/**/*.{test,spec}.ts'],
+    },
+  }
 })
