7537d2e5eb370e736e30f6498828c0efdc574ff0
Nikolay Kushner <nickolaikushner@gmail.com>
2024-07-28 21:38:26 +0300

    fixes

--- src/server/models/Profile.ts
+++ src/server/models/Profile.ts
@@ -6,12 +6,10 @@ export class Profile implements IProfile {
   name: IProfile['name']
   email: IProfile['email']
   forceUpdatePassword: IProfile['forceUpdatePassword']
-  forceUpdateEmail: IProfile['forceUpdateEmail']
 
   constructor(profile: Partial<IProfile> = {}) {
     this.name = profile.name || faker.person.fullName()
     this.email = profile.email || faker.internet.email()
-    this.forceUpdatePassword = profile.forceUpdatePassword || true
-    this.forceUpdateEmail = true
+    this.forceUpdatePassword = profile.forceUpdatePassword ?? true
   }
 }
