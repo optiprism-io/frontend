8670244505de1935652e1fa5562b2735d65d62d5
kkulebaev <konstantinkulebaev@gmail.com>
2024-08-27 10:46:42 +0300

    feat(conversion-over-time): add table

--- src/components/funnels/view/conversion-over-time/ConversionOverTime.vue
+++ src/components/funnels/view/conversion-over-time/ConversionOverTime.vue
@@ -4,18 +4,34 @@
     @change-view="emit('change-view', $event)"
   >
     <template #chart>
-      <div>CHART</div>
+      <pre> {{ reportConversion }} </pre>
     </template>
 
     <template #table>
-      <div>TABLE</div>
+      <ConversionOverTimeTable
+        v-if="reportConversion"
+        :report-conversion="reportConversion"
+      />
     </template>
   </FunnelContentGrid>
 </template>
 
 <script setup lang="ts">
+import { computed, nextTick, ref, watch } from 'vue'
+
+import ConversionOverTimeTable from '@/components/funnels/view/conversion-over-time/ConversionOverTimeTable.vue'
 import FunnelContentGrid from '@/components/funnels/view/FunnelContentGrid.vue'
 
+import { FunnelConversionOverTimeChartTypeTypeEnum, FunnelQueryCountEnum } from '@/api'
+import { apiClient } from '@/api/apiClient'
+import { useMutation } from '@/hooks/useMutation'
+import { TimeTypeEnum, usePeriod } from '@/hooks/usePeriod'
+import { useStepsStore } from '@/stores/funnels/steps'
+import { useProjectsStore } from '@/stores/projects/projects'
+import { useBreakdownsStore } from '@/stores/reports/breakdowns'
+import { useFilterGroupsStore } from '@/stores/reports/filters'
+
+import type { EventRecordsListRequestTime, FunnelResponseStepsInner } from '@/api'
 import type { FunnelChartType } from '@/pages/reports/funnelViews'
 
 interface IProps {
@@ -27,4 +43,83 @@ withDefaults(defineProps<IProps>(), {})
 const emit = defineEmits<{
   (e: 'change-view', payload: FunnelChartType): void
 }>()
+
+interface Period {
+  from: string
+  to: string
+  last: number
+  type: TimeTypeEnum
+}
+
+const reportConversion = ref<FunnelResponseStepsInner | undefined>()
+
+const stepsStore = useStepsStore()
+const projectsStore = useProjectsStore()
+const filterGroupsStore = useFilterGroupsStore()
+const breakdownsStore = useBreakdownsStore()
+
+const { mutate: getReports } = useMutation(fetchReports)
+
+const controlsPeriod = ref<string | number>('30')
+const period = ref<Period>({
+  from: '',
+  to: '',
+  type: TimeTypeEnum.Last,
+  last: 30,
+})
+
+const timeRequest = computed<EventRecordsListRequestTime>(() => {
+  const { getRequestTime } = usePeriod()
+  return getRequestTime(
+    period.value.type,
+    controlsPeriod.value,
+    period.value.from,
+    period.value.to,
+    period.value.last
+  )
+})
+
+async function fetchReports(): Promise<void> {
+  /* need nextTick for update stepsStore.getSteps */
+  await nextTick()
+  const steps = stepsStore.getSteps
+
+  const res = await apiClient.query.funnelQuery(projectsStore.projectId, {
+    steps,
+    time: timeRequest.value,
+    group: stepsStore.group,
+    breakdowns: breakdownsStore.breakdownsItems,
+    filters: filterGroupsStore.filters,
+    timeWindow: {
+      n: stepsStore.size,
+      unit: stepsStore.unit,
+    },
+    chartType: {
+      type: FunnelConversionOverTimeChartTypeTypeEnum.ConversionOverTime,
+      intervalUnit: 'day',
+    },
+    count: FunnelQueryCountEnum.NonUnique,
+    touch: {
+      type: 'first',
+    },
+    exclude: stepsStore.getExcluded,
+    holdingConstants: stepsStore.getHoldingProperties,
+  })
+
+  if (res?.data) {
+    reportConversion.value = res.data.steps.at(-1)
+  }
+}
+
+watch(() => [stepsStore, filterGroupsStore, breakdownsStore, timeRequest], getReports, {
+  deep: true,
+  immediate: true,
+})
 </script>
+
+<style lang="scss" scoped>
+pre {
+  height: 40rem;
+  overflow: auto;
+}
+</style>
---
+++ src/components/funnels/view/conversion-over-time/ConversionOverTimeTable.vue
@@ -0,0 +1,93 @@
+<template>
+  <UiDataTable
+    :columns="columns"
+    :data="data"
+    :scroll-x="scrollX"
+  />
+</template>
+
+<script setup lang="ts">
+import { computed } from 'vue'
+
+import UiDataTable from '@/components/uikit/UiDataTable.vue'
+
+import { DEFAULT_SEPARATOR } from '@/constants'
+import { getYYYYMMDD } from '@/helpers/getStringDates'
+
+import type { FunnelResponseStepsInner, FunnelResponseStepsInnerDataInner } from '@/api'
+import type { RowData, TableColumn } from 'naive-ui/es/data-table/src/interface'
+
+interface IProps {
+  reportConversion: FunnelResponseStepsInner | undefined
+}
+
+interface PreparedData {
+  [group: string]: {
+    [ts: number]: number
+  }
+}
+
+const props = withDefaults(defineProps<IProps>(), {})
+
+const preparedData = computed(() => {
+  const res: PreparedData = {}
+
+  if (!props.reportConversion) return []
+
+  for (let i = 0; i < props.reportConversion.data.length; i++) {
+    const el: FunnelResponseStepsInnerDataInner = props.reportConversion.data[i]
+    const groupString = el.groups.join(DEFAULT_SEPARATOR)
+    if (!res[groupString]) res[groupString] = {}
+    res[groupString][el.ts] = el.conversionRatio
+  }
+
+  return res
+})
+
+const data = computed<RowData[]>(() => {
+  const res: RowData[] = Object.values(preparedData.value)
+
+  for (let i = 0; i < res.length; i++) {
+    const group = Object.keys(preparedData.value)[i]
+    res[i].name = group
+  }
+
+  return res
+})
+
+const columns = computed(() => {
+  const columns: TableColumn[] = []
+  const timestamps: Set<number> = new Set()
+
+  Object.values(preparedData.value).forEach(data => {
+    Object.keys(data).forEach(ts => timestamps.add(+ts))
+  })
+
+  Array.from(timestamps)
+    .toSorted((a, b) => a - b)
+    .forEach(ts => {
+      columns.push({
+        key: ts,
+        title: getYYYYMMDD(new Date(ts)),
+        resizable: true,
+        render: (rowData: RowData) => {
+          const value = rowData[ts] || 0
+          return value + '%'
+        },
+      })
+    })
+
+  columns.unshift({
+    key: 'name',
+    title: 'Groups',
+    resizable: true,
+  })
+
+  return columns
+})
+
+const scrollX = computed(() => {
+  const WIDTH_ONE_COLUMN = 250 // value calculated experimentally
+  return columns.value.length * WIDTH_ONE_COLUMN
+})
+</script>
