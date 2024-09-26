0c7a99a1928663586a9aaa3be047e27ebb31c860
Konstantin Kulebaev <konstantinkulebaev@gmail.com>
2024-09-26 13:39:30 +0300

    fix: remove flex style for container (#111)

--- src/components/uikit/UiCard/UiCardContainer.vue
+++ src/components/uikit/UiCard/UiCardContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="pf-c-card pf-m-compact pf-u-h-100 overflow-auto pf-l-flex">
+  <div class="pf-c-card pf-m-compact pf-u-h-100 overflow-auto">
     <slot />
   </div>
 </template>
